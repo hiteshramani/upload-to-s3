c4db4ff614136d7ce2f556d3a29e5f8cc968acee
hitesh.ramani <hiteshramani@hotmail.com>
2018-05-10 16:06:39 +0530

    added settings

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+
+# s3 env variables
+S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
+S3_KEY = os.environ.get("S3_ACCESS_KEY")
+S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# db env variable
+SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI')
