6a67d4793000f6b1792c61ec72a12d970a5ceb06
hitesh.ramani <hiteshramani@hotmail.com>
2018-05-10 16:09:40 +0530

    updated timezone

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 # Import the database object (db) from the main application module
 # We will define this inside /app/__init__.py in the next sections.
 from app import db
+from datetime import datetime
 
 
 # Define a base model for other database tables to inherit
@@ -12,7 +13,6 @@ class S3File(db.Model):
     mimeType = db.Column(db.String)
     webLink = db.Column(db.String)
     size = db.Column(db.BigInteger)
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                           onupdate=db.func.current_timestamp())
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
     is_private = db.Column(db.Boolean, default=False)
